b64c1a9f2f55e8b3c89cdd38524270d3c2aa8ec3
Evgeny Chistyakov <chist.evg@gmail.com>
2024-03-02 13:52:00 +0100

    Add an implementation of the Unpack() function

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,65 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var resultString strings.Builder
+	var currentLetter rune
+	var count int
+
+	if len(s) == 0 {
+		return "", nil
+	}
+
+	if _, err := strconv.Atoi(string(s[0])); err == nil {
+		return "", ErrInvalidString
+	}
+
+	for _, letter := range s {
+		var err error
+		if currentLetter == 0 && count != 0 {
+			_, err = strconv.Atoi(string(letter))
+			if err == nil {
+				return "", ErrInvalidString
+			}
+		}
+
+		if currentLetter == 0 {
+			count, err = strconv.Atoi(string(letter))
+			if err != nil {
+				currentLetter = letter
+				continue
+			}
+			return "", ErrInvalidString
+		}
+
+		count, err = strconv.Atoi(string(letter))
+		if err != nil {
+			_, err := resultString.WriteRune(currentLetter)
+			if err != nil {
+				return "", ErrInvalidString
+			}
+			currentLetter = letter
+			count = 0
+			continue
+		}
+
+		_, err = resultString.WriteString(strings.Repeat(string(currentLetter), count))
+		if err != nil {
+			return "", ErrInvalidString
+		}
+		currentLetter = 0
+	}
+
+	_, err := resultString.WriteRune(currentLetter)
+	if err != nil {
+		return "", ErrInvalidString
+	}
+
+	return resultString.String(), nil
 }

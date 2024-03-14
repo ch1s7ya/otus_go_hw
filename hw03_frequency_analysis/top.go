0e3320b52b306da71414f6270b5ed27fdfd4759a
Evgenii Chistiakov <chist.evg+git@gmail.com>
2024-03-14 21:46:25 +0100

    Start hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,28 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
+import (
+	"fmt"
+	"strings"
+)
+
+func Top10(text string) []string {
+	words := strings.Fields(text)
+	fmt.Println(words[0])
+	return nil
+}
+
+func buildFrequencyListOfWords(words []string) map[string]int {
+	make frequenceListOfWords 
+	for word := range words {
+
+	}
+	return nil
+}
+
+func getTop10(words []string) []string {
+	return nil
+}
+
+func sortWords(words []string) []string {
 	return nil
 }

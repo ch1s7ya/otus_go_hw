4458743b8852410bb1ecf0f389edb9f27d30b91f
Evgenij Chistyakov <chist.evg@gmail.com>
2024-04-24 21:55:15 +0200

    Implement doubly linked list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,9 +17,9 @@ type ListItem struct {
 }
 
 type list struct {
-	head *ListItem
-	tail *ListItem
-	len  int
+	front *ListItem
+	back  *ListItem
+	len   int
 }
 
 func NewList() List {
@@ -31,13 +31,70 @@ func (l list) Len() int {
 }
 
 func (l list) Front() *ListItem {
-	return l.head
+	if l.len == 0 {
+		return nil
+	}
+	return l.front
 }
 
 func (l list) Back() *ListItem {
-	return l.tail
+	if l.len == 0 {
+		return nil
+	}
+	return l.back
 }
 
-func (l list) PushFront(v interface{}) *ListItem {
-	
+func (l *list) PushFront(v interface{}) *ListItem {
+	e := &ListItem{v, nil, nil}
+	if l.front == nil {
+		l.front = e
+		l.back = e
+	} else {
+		l.front.Prev = e
+		e.Next = l.front
+		e.Prev = nil
+		l.front = e
+	}
+	l.len++
+	return e
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	e := &ListItem{v, nil, nil}
+	if l.back == nil {
+		l.front = e
+		l.back = e
+		l.len++
+	} else {
+		l.back.Next = e
+		e.Next = nil
+		e.Prev = l.back
+		l.back = e
+	}
+	l.len++
+	return e
+}
+
+func (l *list) Remove(i *ListItem) {
+	i.Next.Prev = i.Prev
+	i.Prev.Next = i.Next
+	i.Next = nil
+	i.Prev = nil
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == l.Front() {
+		return
+	}
+	if i != l.Back() {
+		i.Next.Prev = i.Prev
+	}
+	i.Prev.Next = i.Next
+
+	i.Next = l.front
+	i.Prev = l.front.Prev
+
+	i.Next.Prev = i
+	l.front = i
 }

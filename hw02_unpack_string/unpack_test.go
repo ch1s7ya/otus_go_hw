ac2e66f85fab77ec08edf86432015d498b51cd36
Evgeny <chist.evg@gmail.com>
2024-08-15 10:06:07 +0200

    Update unpack_test.go
    Add test case with "-"

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -25,6 +25,7 @@ func TestUnpack(t *testing.T) {
 		{input: `qwe\\5`, expected: `qwe\\\\\`},
 		{input: `qwe\\\3`, expected: `qwe\3`},
 		{input: `qwe\\`, expected: `qwe\`},
+		{input: `--`, expected: `--`},
 	}
 
 	for _, tc := range tests {
